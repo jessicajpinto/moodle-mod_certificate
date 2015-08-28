0a5d271ac68b58122b3eef2c8109ae3f3da8b34f
José Eduardo Chaves Costa <zeduardu@gmail.com>
2015-08-28 11:57:50 -0300

    Implementação
    Ajuste do texto para quando for tutor de conteúdo de uma unidade
    específica.

--- type/A4_Tutor_Conteudo/certificate.php
+++ type/A4_Tutor_Conteudo/certificate.php
@@ -104,8 +104,12 @@ if ($certificate->printhours) {
  else {
      $carga_horaria = 0;
 }
-$texto_certificacao = "Certificamos que o(a) Sr(a) $nome_completo atuou como Tutor de Conteúdo$certificate->customtext do curso $course->fullname, promovido pela Divisão de Desenvolvimento de Pessoal - DIDEP/DDRH/PROGEP, no período de $data_inicio a $data_final, com carga horária total de $carga_horaria horas.";
-certificate_print_text($pdf, $x + 10, $y + 80, 'J', 'Times', '', 15, $texto_certificacao, 258);
+if ($certificate->customtext) {
+    certificate_print_text($pdf, $x + 10, $y + 80, 'J', 'Times', '', 15, sprintf($certificate->customtext, $nome_completo), 258);
+} else {
+    $texto_certificacao = "Certificamos que o(a) Sr(a) $nome_completo atuou como Tutor de Conteúdo do curso $course->fullname, promovido pela Divisão de Desenvolvimento de Pessoal - DIDEP/DDRH/PROGEP, no período de $data_inicio a $data_final, com carga horária total de $carga_horaria horas.";
+    certificate_print_text($pdf, $x + 10, $y + 80, 'J', 'Times', '', 15, $texto_certificacao, 258);
+}
 
 // Local e data de impressão do certificado
 $data_impressao = strftime('%d de %B de %Y', certificate_get_date_unformated($certificate, $certrecord, $course));
